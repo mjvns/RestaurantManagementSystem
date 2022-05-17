275bbdb54b79ea6262ac4d97029d66cb74e837b3
Madhukar Jaiswal <Madhukar.Jaiswal.Contractor@pepsico.com>
2022-05-17 23:30:57 +0530

    Implemented functionality to list available ingredients

--- src/RMS.java
+++ src/RMS.java
@@ -1,5 +1,21 @@
+import com.rms.inventorymanagement.Inventory;
+
+import java.util.Scanner;
+
 public class RMS {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Welcome to Restaurant Management System...");
+        System.out.println("Press 1 ---> View Available Ingredients");
+        System.out.println("Press 2 ---> Order Specific Ingredients");
+        System.out.println("Press 3 ---> View Total Sales");
+
+        byte userInput = scan.nextByte();
+
+        switch(userInput){
+            case 1:
+                Inventory.listAvailableIngredients();
+        }
     }
 }
--- src/com/rms/inventorymanagement/Inventory.java
+++ src/com/rms/inventorymanagement/Inventory.java
@@ -2,12 +2,24 @@ package com.rms.inventorymanagement;
 
 import com.rms.entities.IngredientQuantity;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Inventory {
-    private List<IngredientQuantity> stock;
+    private static List<IngredientQuantity> stock;
 
-    public List<IngredientQuantity> getStock() {
+    private static List<IngredientQuantity> getStock() {
         return stock;
+    }
+
+    public static void listAvailableIngredients(){
+        List<IngredientQuantity> availableIngredients = getStock();
+        Iterator<IngredientQuantity> itr = availableIngredients.listIterator();
+
+        while (itr.hasNext()){
+            IngredientQuantity ingredientInfo = itr.next();
+            System.out.println("Ingredient Name: "+ ingredientInfo.getIngredient().getName() + "\n" +
+                                "Quantity in stock: "+ ingredientInfo.getQuantity());
+        }
     }
 }
